04157b46aa9d988008b8d824cf0352a9d9096128
FokoHetman <paprykkania@gmail.com>
2025-01-20 15:45:58 +0100

    movement independent of fps (multiplying times delta)

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 
-const speed = 50
+const speed = 5000
 
 
 
@@ -16,17 +16,17 @@ func player_movement(delta):
 
 
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
+		velocity.x = speed*delta
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -speed
+		velocity.x = -speed*delta
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
 		velocity.x = 0
-		velocity.y = -speed
+		velocity.y = -speed*delta
 	elif Input.is_action_pressed("ui_down"):
 		velocity.x = 0
-		velocity.y = speed
+		velocity.y = speed*delta
 	else:
 		velocity.x = 0
 		velocity.y = 0
