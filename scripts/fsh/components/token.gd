d95b99a38d2d4ccbe61d98fa447e0b3f4fe5a798
FokoHetman <paprykkania@gmail.com>
2025-01-20 18:01:17 +0100

    Tokenizer (full)

--- scripts/fsh/components/token.gd
+++ scripts/fsh/components/token.gd
@@ -1,37 +1,13 @@
 extends Node
 class_name Token
 
-enum Operator {
-	Addition,			# +
-	Substraction,		# -
-	Multiplication,		# *
-	Division			# /
-}
 
-enum TokenType {
-	Identifier,			# __function__ x y 
-	Integer,			# 5
-	Float,				# 6.8
-
-	Operator,			# Operator enum
-	OParen,				# (
-	CParen,				# )
-	OCParen,			# {
-	CCParen,			# }
-	OSParen,			# [
-	CSParen,			# ]
-	
-	SemiColon,			# ;
-	Pipe,				# |
-	LBracket,			# <
-	RBracket,			# >
-}
-
-var type: TokenType
-var operator: Operator	# nullable
+var type
+var value				# any (String, Int, Null, depends ok TokenType)
+var operator			# nullable
 
 
-func _init(type, operator=null):
-	type = type
-	operator = operator
-	self
+func _init(ptype, pvalue, poperator):
+	type = ptype
+	value = pvalue
+	operator = poperator
--- scripts/fsh/tokenizer.gd
+++ scripts/fsh/tokenizer.gd
@@ -2,7 +2,39 @@
 extends Node
 class_name Tokenizer
 
+enum Operator {
+	Addition,			# +
+	Substraction,		# -
+	Multiplication,		# *
+	Division			# /
+}
 
+enum TokenType {
+	Identifier,			# __function__ x y 
+	String,				# "hello there"
+	Integer,			# 5
+	Float,				# 6.8
+
+	Operator,			# Operator enum
+	OParen,				# (
+	CParen,				# )
+	OCParen,			# {
+	CCParen,			# }
+	OSParen,			# [
+	CSParen,			# ]
+	
+	SemiColon,			# ;
+	Pipe,				# |
+	LBracket,			# <
+	RBracket,			# >
+	
+	Quote,				# "
+	LQuote,				# '
+}
+
+enum TokenizerError {
+	UnmatchedQuote
+}
 
 func chars(str: String) -> Array:
 	var array = []
@@ -16,32 +48,84 @@ func _init():
 
 func tokenize(code: String):
 	var chars = chars(code)
-	while chars.len()>0:
+	var tokens = []
+	while len(chars)>0:
 		var removable = true
 		match chars[0]:
-			
+			"<":
+				tokens.append(Token.new(TokenType.RBracket, null, null))
+			">":
+				tokens.append(Token.new(TokenType.RBracket, null, null))
+			"|":
+				tokens.append(Token.new(TokenType.Pipe, null, null))
+			";":
+				tokens.append(Token.new(TokenType.SemiColon, null, null))
+			"]":
+				tokens.append(Token.new(TokenType.CSParen, null, null))
+			"[":
+				tokens.append(Token.new(TokenType.OSParen, null, null))
+			"}":
+				tokens.append(Token.new(TokenType.CCParen, null, null))
+			"{":
+				tokens.append(Token.new(TokenType.OCParen, null, null))
+			")":
+				tokens.append(Token.new(TokenType.CParen, null, null))
+			"(":
+				tokens.append(Token.new(TokenType.OParen, null, null))
+			"+":
+				tokens.append(Token.new(TokenType.Operator, null, Operator.Addition))
+			"-":
+				tokens.append(Token.new(TokenType.Operator, null, Operator.Substraction))
+			"*":
+				tokens.append(Token.new(TokenType.Operator, null, Operator.Multiplication))
+			"/":
+				tokens.append(Token.new(TokenType.Operator, null, Operator.Division))
 			"\"":
-				pass
+				chars.remove_at(0)
+				var buffer = ""
+				while chars[0]!="\"":
+					buffer += chars[0]
+					chars.remove_at(0)
+					if len(chars)==0:
+						return [false, TokenizerError.UnmatchedQuote]
+				tokens.append(Token.new(TokenType.String, buffer, null))
+			"\'":
+				chars.remove_at(0)
+				var buffer = ""
+				while chars[0]!="\"":
+					buffer += chars[0]
+					chars.remove_at(0)
+					if len(chars)==0:
+						return [false, TokenizerError.UnmatchedQuote]
+				tokens.append(Token.new(TokenType.String, buffer, null))
 			_:
 				if chars[0].is_valid_int():
 					var buffer = chars[0]
-					chars.remove(0)
-					while (buffer + chars[0]).is_valid_int():
+					chars.remove_at(0)
+					while len(chars)>0 && (buffer + chars[0]).is_valid_int():
 						buffer += chars[0]
-						chars.remove(0)
-					if chars[0]==".":
+						chars.remove_at(0)
+					if len(chars)>0 && chars[0]==".":
 						buffer += chars[0]
-						chars.remove(0)
-						while (buffer+chars[0]).is_valid_float():
+						chars.remove_at(0)
+						while len(chars)>0 && (buffer+chars[0]).is_valid_float():
 							buffer += chars[0]
-							chars.remove(0)
+							chars.remove_at(0)
+						tokens.append(Token.new(TokenType.Float, float(buffer), null))
+					else:
+						tokens.append(Token.new(TokenType.Integer, int(buffer), null))
 					removable = false
-				if chars[0].is_valid_identifier():
+
+
+				if len(chars)>0 && chars[0].is_valid_identifier():
 					var buffer = chars[0]
-					chars.remove(0)
+					chars.remove_at(0)
 					while (buffer + chars[0]).is_valid_identifier():
 						buffer += chars[0]
-						chars.remove(0)
+						chars.remove_at(0)
 					removable = false
+					tokens.append(Token.new(TokenType.Identifier, buffer, null))
+
 		if removable:
-			chars.remove(0)
+			chars.remove_at(0)
+	return [true, tokens]
