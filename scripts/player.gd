fc62e824370a869e0ed7d11908f3d0750624655f
FokoHetman <paprykkania@gmail.com>
2025-01-25 16:22:06 +0100

    movement change

--- scripts/player.gd
+++ scripts/player.gd
@@ -24,6 +24,7 @@ func jump(timer) -> void:
 	move_and_slide()
 	
 var running = false
+var last_movement = false		# false - x; true - y
 
 func player_movement(delta):
 	
@@ -46,8 +47,11 @@ func player_movement(delta):
 		elif Input.is_key_pressed(KEY_W) || Input.is_key_pressed(KEY_S)|| Input.is_key_pressed(KEY_A)|| Input.is_key_pressed(KEY_D):
 			var dy = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))  # zmiana w x = WCIŚNIĘTY W - WCIŚNIĘTY S (W = 1; S = -1; W+S=0)
 			var dx = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A)) # zmiana w y = WCIŚNIĘTY A - WCIŚNIĘTY D (A = 1; D = -1; A+D=0)
-			dy *= int(dx==0)                                                              # brak zmiany w y jeśli nastąpiła zmiana w x, można zamienić `dy` z `dx` aby uzyskać odwrotny rezultat
-
+			if last_movement:
+				dy *= int(dx==0)                                                              # brak zmiany w y jeśli nastąpiła zmiana w x, można zamienić `dy` z `dx` aby uzyskać odwrotny rezultat
+			else:
+				dx *= int(dy==0)
+			last_movement = abs(dx) > abs(dy)
 			velocity.x = dx * (SPEED * (1 + int(running))) * delta
 			velocity.y = dy * (SPEED * (1 + int(running))) * delta
 		else:
