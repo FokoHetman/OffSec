0d4022dcb671b2f9052c471039b4721dd7154d63
FokoHetman <paprykkania@gmail.com>
2025-01-25 15:06:53 +0100

    proper movement + directory naming

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,16 +22,15 @@ func jump(timer) -> void:
 		velocity.y -= GRAVITY*10
 	stage += 1
 	move_and_slide()
-	print(stage)
 	
-var running = 0
+var running = false
 
 func player_movement(delta):
 	
 	if Input.is_key_pressed(KEY_SHIFT):
-		running = 2000
+		running = true
 	else:
-		running = 0
+		running = false
 		
 	if not jumping:
 		if Input.is_key_pressed(KEY_SPACE):
@@ -44,27 +43,16 @@ func player_movement(delta):
 			add_child(timer)
 			$CollisionShape2D.set_deferred("disabled",true)
 			jumping = true
-			
-		elif Input.is_key_pressed(KEY_D):
-			velocity.x = SPEED*delta+running
-			velocity.y = 0
-			
-		elif Input.is_key_pressed(KEY_A):
-			velocity.x = -SPEED*delta-running
-			velocity.y = 0
-			
-		elif Input.is_key_pressed(KEY_W):
-			velocity.x = 0
-			velocity.y = -SPEED*delta-running
-			
-		elif Input.is_key_pressed(KEY_S):
-			velocity.x = 0
-			velocity.y = SPEED*delta+running
-			
+		elif Input.is_key_pressed(KEY_W) || Input.is_key_pressed(KEY_S)|| Input.is_key_pressed(KEY_A)|| Input.is_key_pressed(KEY_D):
+			var dy = int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))  # zmiana w x = WCIŚNIĘTY W - WCIŚNIĘTY S (W = 1; S = -1; W+S=0)
+			var dx = int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A)) # zmiana w y = WCIŚNIĘTY A - WCIŚNIĘTY D (A = 1; D = -1; A+D=0)
+			dy *= int(dx==0)                                                              # brak zmiany w y jeśli nastąpiła zmiana w x, można zamienić `dy` z `dx` aby uzyskać odwrotny rezultat
+
+			velocity.x = dx * (SPEED * (1 + int(running))) * delta
+			velocity.y = dy * (SPEED * (1 + int(running))) * delta
 		else:
 			velocity.x = 0
 			velocity.y = 0
-			
 	else:
 		velocity.x = 0
 		velocity.y = 0
