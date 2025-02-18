e22aa1404448757d91467cf84d64943c5c13bdaf
FokoHetman <paprykkania@gmail.com>
2025-02-18 22:42:34 +0100

    lantern mouse [wind]

--- scripts/world/fauna/lantern_mouse.gd
+++ scripts/world/fauna/lantern_mouse.gd
@@ -3,10 +3,8 @@ extends Node2D
 const max_string_len = 100
 
 enum SPRITE_DIRECTION {
-	DOWN,
-	LEFT, # no impl vvv
-	RIGHT,
-	UP
+	Y_AXIS,
+	X_AXIS
 }
 
 
@@ -33,24 +31,31 @@ func _ready():
 
 func sprite(state):
 	match state:
-		SPRITE_DIRECTION.DOWN:
+		SPRITE_DIRECTION.Y_AXIS:
 			$poly_down.visible = true
+		SPRITE_DIRECTION.X_AXIS:
+			$poly_down.visible = false
 
 func hang():
-	sprite(SPRITE_DIRECTION.DOWN)
+	sprite(SPRITE_DIRECTION.Y_AXIS)
 	$skeleton_down/tail.position = lerp($skeleton_down/tail.position, attachment_point+Vector2(0, max_string_len), 0.1) 
-	if tick>35:
+	if tick>160:
 		wind()
-	if tick%2==0:
-		gravity()
+	gravity()
 
 var inertia = 0
 var base_inertia = 0
+
+var can_spawn_wind_visual = true
+
 func wind():
 	var wind_val = $Weather.get_wind_at(get_parent().name, $skeleton_down/tail/body/head.position)
 	$skeleton_down/tail.position = lerp($skeleton_down/tail.position, $skeleton_down/tail.position+wind_val, 0.1)
 	inertia += wind_val.x * 0.1
 	base_inertia += wind_val.x * 0.1
+	
+	if can_spawn_wind_visual:
+		can_spawn_wind_visual = !($Weather.spawn_wind_object($skeleton_down/tail.position - 5*wind_val + Vector2(0,50)))
 
 func gravity():
 	$skeleton_down/tail.position.x = lerp($skeleton_down/tail.position.x, $skeleton_down/tail.position.x-inertia*2.3, 0.1)
@@ -59,19 +64,28 @@ func gravity():
 		base_inertia = 0
 		inertia = 0
 	if inertia==0 && base_inertia==0:
-		$skeleton_down/tail.position.x = lerp($skeleton_down/tail.position.x, attachment_point.x, 0.4)
+		$skeleton_down/tail.position.x = lerp($skeleton_down/tail.position.x, attachment_point.x, 0.35)
 	
 	# $skeleton_down/tail.position
 	# attachment_point
 	var dy = abs($skeleton_down/tail.position.y - attachment_point.y)
 	var dx = abs($skeleton_down/tail.position.x - attachment_point.x)
-	print(dx, dy, atan(dy/dx)- deg_to_rad(90))
 	$skeleton_down/tail.rotation = atan(dy/dx)- deg_to_rad(90)
-	
+
+
+func find_attachment_point():
+	pass # it will never do that
+
+
+
+
 var tick = 0
+var reset = randf()*600
 func _process(d):
-	if tick>80:
+	if tick>reset:#220:
+		reset = randf()*600
 		tick = 0
+		can_spawn_wind_visual = true
 	if tick%2==0:
 		match goal:
 			AI_GOALS.Hang:
--- scripts/world/weather.gd
+++ scripts/world/weather.gd
@@ -1,8 +1,33 @@
 extends Node2D
 
 
+var wind_texture = preload("res://art/wind.png")
+
 func get_wind_at(world, position):
 	return Vector2(50,0)#return Vector2((abs(position.x)+abs(position.y))/10, 0) # this code is so ass
+
+
+var iteration = 0
+func wind_object_movement(timer, poly):
+	if iteration>=100:
+		iteration = 0
+		poly.queue_free()
+		timer.queue_free()
+	iteration += 1
+	poly.position = lerp(poly.position, poly.position + 2*get_wind_at(get_parent().name, poly.position), 0.05)
+
+func spawn_wind_object(at: Vector2):
+	var poly = Sprite2D.new()
+	poly.texture = wind_texture
+	poly.position = at
+	add_child(poly)
+	var timer = Timer.new()
+	timer.one_shot = false
+	timer.wait_time = 0.01
+	timer.autostart = true
+	timer.connect("timeout", Callable(self, "wind_object_movement").bind(timer, poly))
+	add_child(timer)
+	return poly
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
