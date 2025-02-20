735521438d16d4943158bc2da9f27742d256eb6b
FokoHetman <paprykkania@gmail.com>
2025-02-20 18:06:40 +0100

    map update

--- scripts/world/rooms/world_map.gd
+++ scripts/world/rooms/world_map.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 
 var heat_flower_scene = preload("res://scenes/flora/heat_flower.tscn")
+var mouse_scene = preload("res://scenes/fauna/lantern_mouse.tscn")
+
+var flower_positions = []#[Vector2(200,200)]
+var mice_positions = []#[Vector2(200,200)]
+var lliljana_positions = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -11,7 +16,15 @@ func _ready():
 	add_to_group("persistence", true)
 	if get_parent().has_method("refresh") && get_parent().refresh()["current_scene"].to_lower()=="player_room":
 		get_parent().spawn_player(Vector2(220,200))
-
+	
+	for i in flower_positions:
+		var hfs = heat_flower_scene.instantiate()
+		hfs.position = i
+		add_child(hfs)
+	for i in mice_positions:
+		var mouse = mouse_scene.instantiate()
+		mouse.position = i
+		add_child(mouse)
 func save():
 	return {
 		"filename": get_scene_file_path()
