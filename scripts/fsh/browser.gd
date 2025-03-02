022bc6ec9e43d09b5d73af8710d043b121290f0a
Jakvb3 <156058145+Jakvb3@users.noreply.github.com>
2025-03-02 19:28:18 +0100

    Update browser.gd

--- scripts/fsh/browser.gd
+++ scripts/fsh/browser.gd
@@ -5,5 +5,5 @@ var icon = preload("res://art/browser.png")
 func show_browser():
 	var browser = Sprite2D.new()
 	browser.texture = icon
-	browser.position = Vector2((randi() % get_viewport().size.x), (randi() % get_viewport().size.y))
+	browser.position = Vector2((get_viewport().size.x - icon.get_size().x)/2, (get_viewport().size.y - icon.get_size().x)/2)
 	add_child(browser)
